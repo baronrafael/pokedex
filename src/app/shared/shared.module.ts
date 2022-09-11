dc35a5f568097600b6ba75500d25b775829abcf2
baronrafael <baronrafael@hotmail.com>
2022-09-10 21:33:47 -0400

    feat: navbar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const COMPONENTS = [
+  NavbarComponent
+];
+
+@NgModule({
+  declarations: [
+    ...COMPONENTS
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ...COMPONENTS
+  ]
+})
+export class SharedModule { }
